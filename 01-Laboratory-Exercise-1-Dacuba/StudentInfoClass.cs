c4bbac967ddff3a8e01b179bb278c1296107bcb7
EJDACUBA-cpu <jhoshcunanan@gmail.com>
2025-08-15 17:40:44 +0800

    Added StudentInfoClass delegates, static methods and  static variables

---
+++ 01-Laboratory-Exercise-1-Dacuba/StudentInfoClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Laboratory_Exercise_1_Dacuba
+{
+    internal class StudentInfoClass
+    {
+        public delegate string
+            DelegateText(string value);
+        public delegate long 
+            DelegateNUmber(long  value);
+
+        public static string Fname, Lname, Mname, Address, Program;
+        public static long Age, ContNum, StuNum;
+        
+
+        public static string
+            GetFirstName(string Fname)
+        {  return Fname; }
+
+        public static string 
+            GetLastName(string Fname)
+        { return Fname; }
+
+        public static string
+            GetMiddleName(string Mname)
+        {  return Mname; }
+
+        public static string
+            GetAddress(string Address)
+        { return Address; }
+
+        public static string
+            GetProgram(string Program) 
+        { return Program; }
+
+        public static long
+            GetAge(long Age) 
+        { return Age; }
+
+        public static long
+            GetContactNumber(long ContNum)  
+        { return ContNum; }
+
+        public static long
+            GetStudentNumber(long StuNum)
+        { return StuNum; }
+
+    }
+}
